8c092d6ddfa72afdd6daec6a21fbe405656d0d33
Irene <irenefl897@gmail.com>
2023-03-12 21:27:20 +0100

    test songs

---
+++ tests/Unit/SongTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Song;
+
+class SongTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testCreateSong()
+    {
+        $song = Song::factory()->create([
+            'artist' => 'Lola',
+            'title' => 'Lolita Lola',
+            
+        ]);
+
+        $this->assertDatabaseHas('songs', [
+            'artist' => 'Lola',
+            'title' => 'Lolita Lola',
+            
+        ]);
+    }
+
+    public function testUpdateSong()
+    {
+        $song = Song::factory()->create();
+
+        $song->artist = 'Dolores';
+        $song->save();
+
+        $this->assertDatabaseHas('songs', [
+            'id' => $song->id,
+            'artist' => 'Dolores',
+        ]);
+    }
+
+    public function testDestroySong()
+    {
+        $song = Song::factory()->create();
+
+        $song->delete();
+
+        $this->assertDatabaseMissing('songs', [
+            'id' => $song->id,
+        ]);
+    }
+}
